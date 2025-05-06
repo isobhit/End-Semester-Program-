4c3b4fe62c6d462f49d09d5287a39d95657bc2c2
SHOBHIT <kumarshushobhit@gmail.com>
2025-05-06 12:09:34 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-import re
-d="python is a programming language"
-p="python"
-res=re.match(p,d,re.IGNORECASE)
-if res:
-    print("starts with python")
-else:
-    print("doesn't starts with python")
